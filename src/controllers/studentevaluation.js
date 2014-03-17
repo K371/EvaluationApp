80c0b42b3a47009aa75f475ec99eea69ca1d3360
Keli <thorkell@me.com>
2014-03-17 20:51:18 +0000

    yoyoyo

--- src/controllers/studentevaluation.js
+++ src/controllers/studentevaluation.js
@@ -16,7 +16,7 @@ app.controller("StudentEvaluationController", [
 
  		$scope.courseAns = [];
  		$scope.teacherAns = [];
- 		$scope.ans = [];
+ 		
  		
  		
 
@@ -68,9 +68,60 @@ app.controller("StudentEvaluationController", [
 		}
 
 		$scope.submitEvaluation = function(){
-			console.log($scope.courseAns);
-			console.log($scope.teacherAns);
-			console.log($scope.teacherText);
+			//console.log($scope.courseAns);
+			
+			//console.log($scope.teacherText);
+			var allAnswers = [];
+		
+			for (value in $scope.courseAns){
+				 	
+
+					allAnswers.push({
+						QuestionID: value,
+						TeacherSSN: "",
+						Value: $scope.courseAns[value]
+					});
+					
+			}
+			
+			for (teacher in $scope.teacherAns){
+				var ssn = "";
+				var qID = 0;
+				var tempSSN = "";
+				var tempQID = 0;
+				 	for(value in $scope.teacherAns[teacher]){
+				 
+					 	var str = [];
+					 	str = $scope.teacherAns[teacher][value].split('/');
+					 	if(tempSSN == ""){
+					 		tempSSN = str[2];
+					 		ssn = tempSSN;
+					 	}
+					 	else{
+					 		ssn = tempSSN;
+					 	}
+					 	//ÞESSI QID VERÐA SÖMU OG GÖMLU, VITLAUST
+					 	
+						 	if(tempQID === 0){
+						 		tempQID = parseInt(str[1]);
+						 		qID = tempQID;
+						 	}
+						 	else{
+						 		qID = ++tempQID;
+						 	}
+					 	
+					 	
+					 	
+					 	
+						allAnswers.push({
+							QuestionID: qID,
+							TeacherSSN: ssn,
+							Value: str[0]
+						});
+					}
+			}
+
+			console.log(allAnswers);
 			$scope.showIt = true;
 			//ApiFactory.addEvaluation();
 			//var answers = new Array();
